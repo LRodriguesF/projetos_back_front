a8583e8c1cb0549f287d2216950535bd242e3816
LRodrigues F <lizarodrigues@outlook.com>
2024-05-03 15:53:12 -0300

    Desafio Lisie

--- cypress/e2e/step_definitions/correios.cy.js
+++ cypress/e2e/step_definitions/correios.cy.js
@@ -5,17 +5,17 @@ Given("que estou na página inicial dos Correios", () => {
 });
 
 When("eu realizo uma busca pelo CEP {string}", (cep) => {
-    cy.get('a.info-link').contains('Busca CEP ou Endereço').click()
-    cy.get('#endereco').type(cep);
-    cy.get('#btn_pesquisar').click();
+    cy.get('a.info-link').contains('Busca CEP ou Endereço').click() // clica no campo de busca
+    cy.get('#endereco').type(cep); // insere o cep
+    cy.get('#btn_pesquisar').click(); // clica no botão de pesquisa
 
 });
 
 Then("devo visualizar o logradouro {string}", (logradouro) => {
-    cy.get('td[data-th="Logradouro/Nome"]').should("contain.text", logradouro);
+    cy.get('td[data-th="Logradouro/Nome"]').should("contain.text", logradouro); // resultado de sucesso do endereço
 });
 
 Then("devo visualizar a mensagem de erro {string}", (mensagem) => {
-    cy.get('div#mensagem-resultado.mensagem').should("contain.text", mensagem);
+    cy.get('div#mensagem-resultado.mensagem').should("contain.text", mensagem); // mensagem de enderço inválido
 });
 
